e2b5fdf99e9b148b684e8f0af0e5cad19d0ea21c
Brayan Giraldo <giraldo8900@gmail.com>
2025-03-15 11:08:54 -0500

    none

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function ocultar(){
+    document.getElementById("container").style.display = "none";
+    document.getElementById("form-container").style.display = "flex";
+ }
+
+function Mostrar(){
+    document.getElementById("container").style.display = "block";
+    document.getElementById("form-container").style.display = "none";
+ }
